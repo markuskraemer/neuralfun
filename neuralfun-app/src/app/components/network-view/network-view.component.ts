f398f8d825e3ca91de1401fca6377753d6c7f4fd
markuskraemer <master@nothing8000.de>
2017-11-08 09:14:37 +0100

    introduced connections

--- neuralfun-app/src/app/components/network-view/network-view.component.ts
+++ neuralfun-app/src/app/components/network-view/network-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ApplicationRef } from '@angular/core';
 import { Network } from '../../network/Network';
 
 @Component({
@@ -9,5 +9,31 @@ import { Network } from '../../network/Network';
 export class NetworkViewComponent {
 
   @Input('network') public network:Network;
+
+  constructor (
+    private applicationRef:ApplicationRef
+  ){
+
+  }
+
+  private getPos (id:string):{x:string, y:string}
+  {
+    let elem:HTMLElement = document.querySelector('#neuron_' + id) as HTMLElement;
+    if(elem){
+      let computed:CSSStyleDeclaration = window.getComputedStyle (elem);
+      return {x:elem.offsetLeft + 'px', y:elem.offsetTop + 'px'};
+    }else{
+      setTimeout(()=> this.applicationRef.tick (), 2000);
+      return {x:'0', y:'0'};
+    }
+  }
+
+  private getLineStyle (weight:number):object
+  {
+      return {
+            'stroke': weight > 0 ? 'green' : 'red',
+            'strokeWidth': Math.abs(weight * 2) + 2
+        }
+    }
 
 }
